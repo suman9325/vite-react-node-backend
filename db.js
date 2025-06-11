22ceed1250b118d1f07090cb365493c76c716517
suman1611 <sumankhamaru@os4ed.com>
2025-06-11 19:20:34 +0530

    backend code updated

--- db.js
+++ db.js
@@ -1,18 +1,44 @@
-// db.js
-// require('dotenv').config(); // Load environment variables
+// ------------------------------------------------------------------------------
+// OLD CONNECTION LOCAL
+// ------------------------------------------------------------------------------
+
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   user: 'postgres', // e.g., postgres
+//   host: 'localhost',
+//   database: 'react-vite-db',      // your database name
+//   password: '123',
+//   port: 5432,            // default PostgreSQL port
+// });
+
+// // Test the connection
+// pool.connect()
+//   .then(() => console.log('Connected to PostgreSQL'))
+//   .catch(err => console.error('Connection error', err.stack));
+
+// module.exports = pool;
+
+// ------------------------------------------------------------------------------
+// NEW CONNECTION AWS RDS
+// ------------------------------------------------------------------------------
+
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres', // e.g., postgres
-  host: 'localhost',
-  database: 'react-vite-db',      // your database name
-  password: '123',
-  port: 5432,            // default PostgreSQL port
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
 
-// Test the connection
 pool.connect()
-  .then(() => console.log('Connected to PostgreSQL'))
+  .then(() => console.log('Connected to AWS RDS PostgreSQL'))
   .catch(err => console.error('Connection error', err.stack));
 
 module.exports = pool;
